d7c50f6a542b12439d4df0ad381e9bfc664ef93c
nathaliaspatricio <nathalia.sautchuk@gmail.com>
2022-04-19 16:03:19 +0200

    multilingual support

--- time/actions/actions.py
+++ time/actions/actions.py
@@ -13,10 +13,10 @@ import dateparser
 
 from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.events import EventType
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, UserUtteranceReverted
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
-from db_connectivity import DataUpdate
+from db_connectivity import PersonInsert, CitySelect
 
 city_db = {
     'Brussels': 'Europe/Brussels',
@@ -35,6 +35,12 @@ city_db = {
     'Berlin': 'Europe/Berlin',
 }
 
+ALLOWED_LANGUAGES = {
+    "en": "I want to speak in English",
+    "pt": "Eu quero falar em Português",
+    "de": "Ich will auf Deutsch sprechen",
+}
+
 ALLOWED_EDUCATION_LEVELS = [
     "Ensino Fundamental",
     "Ensino Médio",
@@ -44,6 +50,44 @@ ALLOWED_EDUCATION_LEVELS = [
     "Mestrado",
     "Doutorado",
 ]
+
+class ActionStart(Action):
+
+    def name(self) -> Text:
+        return "action_start"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message(
+            text=f"Which language do you want to speak? \n Em qual idioma você deseja conversar? \n Welche Sprach möchten Sie sprechen?",
+            buttons=[{"title": q, "payload": p} for p,q in ALLOWED_LANGUAGES.items()],
+        )
+        return []
+
+class ActionChooseLanguage(Action):
+
+    def name(self) -> Text:
+        return "action_choose_language"
+
+    def run(
+    self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+
+        current_language = tracker.get_slot("language")
+
+        if current_language == 'pt':
+            msg=f"Então, vamos falar em Português!"
+        elif current_language == 'de':
+            msg=f"Sie sprechen auf Deutsch!"
+        elif current_language == 'en':
+            msg=f"Then, we will speak in English!"
+        else:
+            msg=f"I can't speak this language.\n Eu não falo esse idioma. \n Ich kann diese Spache nicht sprechen."
+
+        dispatcher.utter_message(text=msg)
+        return []
 
 
 class ActionSubmit(Action):
@@ -57,11 +101,19 @@ class ActionSubmit(Action):
         name = tracker.get_slot("name")
         age = tracker.get_slot("age")
         education_level = tracker.get_slot("education_level")
+        current_language = tracker.get_slot("language")
 
-        dispatcher.utter_message(text=f"Eu vou submeter o seu cadastro agora!")
-        msg = f"Seu cadastro:\n - Nome: {name} \n - Idade: {age} anos\n - Nível educacional: {education_level}"  
-        DataUpdate(name, age, education_level)
-        dispatcher.utter_message(text=msg)
+        id = PersonInsert(name, age, education_level)
+
+        if current_language == 'pt':
+            dispatcher.utter_message(text=f"Eu vou submeter o seu cadastro agora!") 
+            dispatcher.utter_message(text=f"Seu cadastro:\n - ID: {id}\n - Nome: {name} \n - Idade: {age} anos\n - Nível educacional: {education_level}")  
+        elif current_language == 'de':
+            dispatcher.utter_message(text=f"Ich werde Ihre Anmeldung jetzt absenden!")
+            dispatcher.utter_message(text=f"Ihre Anmeldung:\n - ID: {id}\n - Vorname: {name} \n - Alter: {age} Jahren\n - Bildungsniveau: {education_level}")  
+        elif current_language == 'en':
+            dispatcher.utter_message(text=f"I will submit your registration now!")
+            dispatcher.utter_message(text=f"Your registration:\n - ID: {id}\n - Name: {name} \n - Age: {age} years\n - Educational Level: {education_level}")  
 
         return []
 
@@ -72,15 +124,25 @@ class AskForEducationLevelAction(Action):
     def run(
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> List[EventType]:
-        #if tracker.get_slot("person_register"):
-        dispatcher.utter_message(
-            text=f"Qual é o seu nível educacional?",
-            buttons=[{"title": p, "payload": p} for p in ALLOWED_EDUCATION_LEVELS],
-        )
-       # else:
-            #dispatcher.utter_message(
-                #text=f"Tchau!",
-            #)
+
+        current_language = tracker.get_slot("language")
+
+        if current_language == 'pt':
+            dispatcher.utter_message(
+                text=f"Qual é o seu nível educacional?",
+                buttons=[{"title": p, "payload": p} for p in ALLOWED_EDUCATION_LEVELS],
+            )
+        elif current_language == 'de':
+            dispatcher.utter_message(
+                text=f"Was ist Ihr Bildungsniveau?",
+                buttons=[{"title": p, "payload": p} for p in ALLOWED_EDUCATION_LEVELS],
+            )
+        elif current_language == 'en':
+            dispatcher.utter_message(
+                text=f"What is your educational level?",
+                buttons=[{"title": p, "payload": p} for p in ALLOWED_EDUCATION_LEVELS],
+            )
+
         return []
 
 class ValidateSimplePersonForm(FormValidationAction):
@@ -96,9 +158,17 @@ class ValidateSimplePersonForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         """Validate `name` value."""
 
+        current_language = tracker.get_slot("language")
+
         if isinstance(slot_value.lower(), str):
-            dispatcher.utter_message(text=f"OK! Seu nome é {slot_value}.")
-            return {"name": slot_value}
+            if current_language == 'pt':
+                dispatcher.utter_message(text=f"OK! Seu nome é {slot_value}.")
+            elif current_language == 'de':
+                dispatcher.utter_message(text=f"OK! Ihre Vorname ist {slot_value}.")
+            elif current_language == 'en':
+                dispatcher.utter_message(text=f"OK! Your name is {slot_value}.")
+            return {"name": slot_value}            
+        
         dispatcher.utter_message(text=f"Por favor, digite apenas caracteres.")
         return {"name": None}
 
@@ -111,7 +181,15 @@ class ValidateSimplePersonForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         """Validate `age` value."""
 
-        dispatcher.utter_message(text=f"OK! Você tem {slot_value} anos.")
+        current_language = tracker.get_slot("language")
+
+        if current_language == 'pt':
+            dispatcher.utter_message(text=f"OK! Você tem {slot_value} anos.")
+        elif current_language == 'de':
+            dispatcher.utter_message(text=f"OK! Sie sind {slot_value} Jahre alt.")
+        elif current_language == 'en':
+            dispatcher.utter_message(text=f"OK! You are {slot_value} years old.")
+
         return {"age": slot_value}
 
     def validate_education_level(
@@ -123,10 +201,18 @@ class ValidateSimplePersonForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         """Validate `education_level` value."""
 
+        current_language = tracker.get_slot("language")
+
         if slot_value not in ALLOWED_EDUCATION_LEVELS:
             dispatcher.utter_message(text=f"Eu não reconheço esse nível educacional. Escolha entre: {', '.join(ALLOWED_EDUCATION_LEVELS)}.")
             return {"education_level": None}
-        dispatcher.utter_message(text=f"OK! Você tem o seguinte nível educacional: {slot_value}.")
+        if current_language == 'pt':
+            dispatcher.utter_message(text=f"OK! Você tem o seguinte nível educacional: {slot_value}.")
+        if current_language == 'de':
+            dispatcher.utter_message(text=f"OK! Sie haben folgendes Bildingsniveau  : {slot_value}.")
+        if current_language == 'en':
+            dispatcher.utter_message(text=f"OK! You have the following educational level: {slot_value}.")
+
         return {"education_level": slot_value}
 
 class ActionRememberWhere(Action):
@@ -138,38 +224,57 @@ class ActionRememberWhere(Action):
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        #if tracker.get_intent_of_latest_message() == "where_i_live":
-            #dispatcher.utter_message(
-                #text=f"Onde você mora?",
-                #buttons=[{"title": p, "payload": p} for p in city_db],
-            #) 
-
+        current_language = tracker.get_slot("language")
         current_place = (next(tracker.get_latest_entity_values("place"), None)).capitalize()
         utc = arrow.utcnow()
 
         if not current_place:
-            msg = f"Eu não entendi onde você vive. Escolha uma das cidades disponíveis"
+            if current_language == 'pt':
+                msg = f"Eu não entendi onde você vive. Escolha uma das cidades disponíveis"
+            elif current_language == 'en':
+                msg = f"I didn't understand where you live. Choose one of the available cities."
+            elif current_language == 'de':
+                msg = f"Ich habe nicht verstanden, wo Sie leben. Wählen Sie eine der verfügbaren Städte aus."
             dispatcher.utter_message(
                 text=msg,
                 buttons=[{"title": p, "payload": p} for p in city_db],
             )    
             return []
 
-        tz_string = city_db.get(current_place, None)
+        #tz_string = city_db.get(current_place, None)
+        tz_string = CitySelect(current_place)
 
         if not tz_string:
-            msg = f"Eu não reconheci {current_place}. Escolha uma das cidades disponíveis"
+            if current_language == 'pt':
+                msg = f"Eu não reconheci {current_place}. Escolha uma das cidades disponíveis"
+            elif current_language == 'en':
+                msg = f"I didn't recognize {current_place}. Choose one of the available cities."
+            elif current_language == 'de':
+                msg = f"Ich habe {current_place} nicht wiedererkannt. Wählen Sie eine der verfügbaren Städte aus."
             dispatcher.utter_message(
                 text=msg,
                 buttons=[{"title": p, "payload": p} for p in city_db],
             )             
-            #msg = f"I didn't recognize {current_place}. Is it spelled correctly?"
             return []
-        
-        msg = f"Ok. Irei me lembrar que você mora em {current_place}."
+
+
+        if current_language == 'pt':
+            msg = f"Ok. Irei me lembrar que você mora em {current_place}."
+        elif current_language == 'en':
+            msg = f"Ok. I will remember that you live in {current_place}."
+        elif current_language == 'de':
+            msg = f"Ok. Ich werde mich daran erinnern, dass Sie in {current_place} leben."
+    
         dispatcher.utter_message(text=msg)
 
-        msg = f"É {utc.to(city_db[current_place]).format('HH:mm')} em {current_place} agora."
+
+        if current_language == 'pt':
+            msg = f"É {utc.to(tz_string).format('HH:mm')} em {current_place} agora."
+        elif current_language == 'en':
+            msg = f"It is {utc.to(tz_string).format('HH:mm')} in {current_place} now."
+        elif current_language == 'de':
+            msg = f"Es ist jetzt {utc.to(tz_string).format('HH:mm')} in {current_place}."
+
         dispatcher.utter_message(text=msg)
 
         return [SlotSet("location", current_place)]
@@ -183,29 +288,52 @@ class ActionTellTime(Action):
     def run(self, dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-         
+        
+        current_language = tracker.get_slot("language") 
         current_place = next(tracker.get_latest_entity_values("place"), None)
+        dispatcher.utter_message(msg="current_place")
         utc = arrow.utcnow()
 
         if not current_place:
-            msg = f"É {utc.format('HH:mm')} utc agora. Você também pode me dar uma localização."
+
+            if current_language == 'pt':
+                msg = f"É {utc.format('HH:mm')} utc agora. Você também pode me dar uma localização."
+            elif current_language == 'en':
+                msg = f"It is {utc.format('HH:mm')} utc now. You can give me a place."
+            elif current_language == 'de':
+                msg = f"Es ist jetzt {utc.format('HH:mm')} utc. Sie können mir auch einen Standort nennen."
+
             dispatcher.utter_message(
                 text=msg,
                 buttons=[{"title": p, "payload": "Moro em " + p} for p in city_db],
             )     
             return []
 
-        tz_string = city_db.get(current_place.capitalize(), None)
+        tz_string = CitySelect(current_place)
+        #tz_string = city_db.get(current_place.capitalize(), None)
 
         if not tz_string:
-            msg = f"Eu não reconheci {current_place}. Escolha uma das cidades disponíveis"
+            if current_language == 'pt':
+                msg = f"Eu não reconheci {current_place}. Escolha uma das cidades disponíveis"
+            elif current_language == 'en':
+                msg = f"I didn't recognize {current_place}. Choose one of the available cities."
+            elif current_language == 'de':
+                msg = f"Ich habe {current_place} nicht wiedererkannt. Wählen Sie eine der verfügbaren Städte aus."
+
             dispatcher.utter_message(
                 text=msg,
-                buttons=[{"title": p, "payload": "Moro em " + p} for p in city_db],
+                #buttons=[{"title": p, "payload": "Moro em " + p} for p in city_db],
             )                 
             return []
 
-        msg = f"É {utc.to(city_db[current_place]).format('HH:mm')} em {current_place} agora."
+        if current_language == 'pt':
+            msg = f"É {utc.to(tz_string).format('HH:mm')} em {current_place} agora."
+        elif current_language == 'en':
+            msg = f"It is {utc.to(tz_string).format('HH:mm')} in {current_place} now."
+        elif current_language == 'de':
+            msg = f"Es ist jetzt {utc.to(tz_string).format('HH:mm')} in {current_place}."
+
+        #msg = f"É {utc.to(city_db[current_place]).format('HH:mm')} em {current_place} agora."
         dispatcher.utter_message(text=msg)
 
         return []
@@ -222,8 +350,16 @@ class ActionTimeDifference(Action):
         timezone_in = (tracker.get_slot("location")).capitalize()
         timezone_to = next(tracker.get_latest_entity_values("place"), None)
         
+        current_language = tracker.get_slot("language")
+
         if not timezone_in:
-            msg = "Para calcular a diferença em horas eu preciso saber onde você mora."
+            if current_language == 'pt':
+                msg = "Para calcular a diferença em horas eu preciso saber onde você mora."
+            elif current_language == 'en':
+                msg = "To calculate the difference in hours, I need to know where you live."
+            elif current_language == 'de':
+                msg = "Um den Studenunterschied zu berechnen, muss ich wissen, wo Sie wohnen."
+
             dispatcher.utter_message(
                 text=msg,
                 buttons=[{"title": p, "payload": "Moro em " + p} for p in city_db],
@@ -231,18 +367,32 @@ class ActionTimeDifference(Action):
             return []            
         
         if not timezone_to:
-            msg = "Eu não entendi com qual timezone você deseja comparar. Escolha uma das disponíveis:"
+
+            if current_language == 'pt':
+                msg = "Eu não entendi com qual timezone você deseja comparar. Escolha uma das disponíveis:"
+            elif current_language == 'en':
+                msg = "I didn't understand which timezone you want to compare with. Choose one of the availables ones:"
+            elif current_language == 'de':
+                msg = "Ich verstehe nicht, mit welcher Zeitzone Sie vergleichen möchten. Wählen Sie eine der verfügbaren aus:"
+
+
             dispatcher.utter_message(
                 text=msg,
                 buttons=[{"title": p, "payload": "Qual a diferença para " + p} for p in city_db],
             )     
-            #msg = "I didn't get the timezone you'd like to compare against. Are you sure it's spelled correctly?"
+
             return []  
 
         tz_string = city_db.get(timezone_to.capitalize(), None)
 
         if not tz_string:
-            msg = f"Eu não reconheci {timezone_to}. Escolha uma das disponíveis:"
+            if current_language == 'pt':
+                msg = f"Eu não reconheci {timezone_to}. Escolha uma das disponíveis:"
+            elif current_language == 'en':
+                msg = f"I didn't recognize {timezone_to}. Choose one of the available ones:"
+            elif current_language == 'de':
+                msg = f"Ich habe {timezone_to} nicht wiedererkannt. Wählen Sie eine der verfügbaren aus:"
+
             dispatcher.utter_message(
                 text=msg,
                 buttons=[{"title": p, "payload": "Qual a diferença para " + p} for p in city_db],
@@ -259,12 +409,29 @@ class ActionTimeDifference(Action):
         diff_hours = int(diff_seconds.seconds/3600)
 
         if diff_hours == 0:
-            msg = f"De " + timezone_in.capitalize() + " para " + timezone_to.capitalize() + ", não há diferença horária."
+            if current_language == 'pt':
+                msg = f"Entre " + timezone_in.capitalize() + " e " + timezone_to.capitalize() + ", não há diferença horária."
+            elif current_language == 'en':
+                msg = f"Among " + timezone_in.capitalize() + " and " + timezone_to.capitalize() + ", there is no time difference."
+            elif current_language == 'de':
+                msg = f"Zwischen " + timezone_in.capitalize() + " und " + timezone_to.capitalize() + " gibt es keinen Zeitunterschied."
+
         elif diff_hours == 1:
-            msg = f"De " + timezone_in.capitalize() + " para " + timezone_to.capitalize() + ", há apenas 1 hora de diferença."
+            if current_language == 'pt':
+                msg = f"Entre " + timezone_in.capitalize() + " e " + timezone_to.capitalize() + ", há apenas 1 hora de diferença."
+            elif current_language == 'en':
+                msg = f"Among " + timezone_in.capitalize() + " and " + timezone_to.capitalize() + ", there is only 1 hour difference."
+            elif current_language == 'de':
+                msg = f"Zwischen " + timezone_in.capitalize() + " und " + timezone_to.capitalize() + " gibt es nur eine Stunde Unterschied."
+
         else:
-            msg = f"De " + timezone_in.capitalize() + " para " + timezone_to.capitalize() + f", há {min(diff_hours, 24-diff_hours)} horas de diferença."
-        #msg = f"From {timezone_in} to {timezone_to}, there is a {min(diff_hours, 24-diff_hours)}H time difference."
+            if current_language == 'pt':
+                msg = f"Entre " + timezone_in.capitalize() + " e " + timezone_to.capitalize() + f", há {min(diff_hours, 24-diff_hours)} horas de diferença."
+            elif current_language == 'en':
+                msg = f"Among " + timezone_in.capitalize() + " and " + timezone_to.capitalize() + f", there is a {min(diff_hours, 24-diff_hours)} hours difference."
+            elif current_language == 'de':
+                msg = f"Zwischen " + timezone_in.capitalize() + " und " + timezone_to.capitalize() + f", gibt es {min(diff_hours, 24-diff_hours)} Stunden Unterschied."
+
         dispatcher.utter_message(text=msg)
 
         return []
--- time/db_connectivity.py
+++ time/db_connectivity.py
@@ -1,12 +1,16 @@
 import mysql.connector
 
-def DataUpdate(name, age, education_level):
+def DbConnection():
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
         passwd="password",
         database="mydatabase"
     )
+    return mydb
+
+def PersonInsert(name, age, education_level):
+    mydb = DbConnection()
 
     mycursor = mydb.cursor()
 
@@ -14,9 +18,34 @@ def DataUpdate(name, age, education_level):
 
     mycursor.execute(sql)
 
-    mydb.commit()
+    sql = 'SELECT LAST_INSERT_ID();'
 
-    print("record inserted")
+    mycursor.execute(sql)
+    row = [item[0] for item in mycursor.fetchall()]
+
+    mydb.commit()
+    mydb.close()
+
+    print("record inserted: " + str(row[0]))
+    return row[0]
+
+
+def CitySelect(city):
+    mydb = DbConnection()
+
+    mycursor = mydb.cursor()
+
+    sql = 'SELECT zone_name FROM time_zone WHERE zone_name LIKE "%' + city + '%" LIMIT 1;'
+
+    mycursor.execute(sql)
+
+    row = [item[0] for item in mycursor.fetchall()]
+
+    mydb.close()
+
+    print("record selected: " + row[0])
+    return row[0]    
+
 
 if __name__=="__main__":
     DataUpdate("Nathalia", 36, "Mestrado")    
